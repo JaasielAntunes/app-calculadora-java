b98aea7d27b403b64de3bef27c0ef5de1509bcd4
Jaasiel Antunes <jasiel.araujo098@gmail.com>
2021-11-30 21:38:51 -0300

    second commit

--- src/main/java/br/edu/fafic/es/classes/Calculadora.java
+++ src/main/java/br/edu/fafic/es/classes/Calculadora.java
@@ -31,4 +31,16 @@ public class Calculadora {
     public double soma() {
         return num1 + num2;
     }
+
+    public double divisao() {
+        return num1 / num2;
+    }
+
+    public double subtracao() {
+        return num1 - num2;
+    }
+
+    public double multiplicacao() {
+        return num1 * num2;
+    }
 }
--- src/main/java/principal/Main.java
+++ src/main/java/principal/Main.java
@@ -6,6 +6,16 @@ public class Main {
     public static void main(String[] args) {
 
         Calculadora calc = new Calculadora(100, 100);
+
         System.out.print("Resultado da soma: " + calc.soma());
+        System.out.println();
+
+        System.out.print("Resultado da divisão: " + calc.divisao());
+        System.out.println();
+
+        System.out.print("Resultado da subtração: " + calc.subtracao());
+        System.out.println();
+
+        System.out.print("Resultado da multiplicação: " + calc.multiplicacao());
     }
 }
